0853debab85fdec0c014bcc35f74ede351d04c67
tiagodev85 <tiagoaragaodev@gmail.com>
2022-12-01 00:52:27 -0300

    Adicionando responsividade

---
+++ tailwind.config.cjs
@@ -0,0 +1,48 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    borderWidth: {
+      "30": "30px",
+      "60": "60px",
+      "20": "20px",
+    },
+    screens: {
+      tablet: "640px",
+      smartphone: "360px",      
+      desktop: "1024px",
+    },
+    fontFamily: {
+      Belleza: ["Belleza"],
+      Angella: ["Angella"],
+    },
+    extend: {
+      width: {
+        "60": "60%",
+        "120": "120px",
+      },
+      height: {
+        meio: "calc(100vh/2)",
+        metade: "512px",
+        "250": "250px",
+      },
+      inset: {
+        meio: "calc(50% - 512px)",
+        controle: "calc(50% - 175px)",
+      },
+      backgroundImage: {
+        "fundo-home": "url('./assets/png_20221129_184952_0000.png')",
+        casal: "url('./assets/AirBrush_20221127204040.jpg')",
+      },
+      colors: {
+        title: "#805730",
+        versiculo: "#613d1a",
+        "button-hover": "#42280e",
+      },
+      textShadow: {
+        default: "8px 6px 3px rgba(255, 255, 255, 0.486)",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-textshadow")],
+};
