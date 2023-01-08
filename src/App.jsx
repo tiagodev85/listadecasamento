fa3bb1348fb203e568ef81a496db1dff766d7211
tiagodev85 <tiagoaragaodev@gmail.com>
2023-01-08 11:19:43 -0300

    Atualizando itens a serem presenteados (retirando link dos itens)

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,13 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Lista from "./pages/Lista";
-import Messages from "./pages/Messages";
 
 function App() {
   return (    
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />                                                     
-          <Route path="/lista" element={<Lista />} />                                             
+          <Route path="/lista" element={<Lista />} />                                                                                                 
         </Routes>
       </Router>         
   );
